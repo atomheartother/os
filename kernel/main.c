3558517dd22a299d83c9ce3d4ac301c2f624b954
Élise <atomheartother@gmail.com>
2020-01-23 02:38:46 +0100

    We lain now

--- kernel/main.c
+++ kernel/main.c
@@ -1,15 +1,16 @@
-int print_vga(const char* str, int line) {
+int print_vga(const char* str, int line, char color) {
     char* vgaMemory = (char*)(0xb8000 + (160 * line));
     int counter = 0;
     while (*str) {
         *vgaMemory = *str;
-        *(vgaMemory + 1) = 0xe;
+        *(vgaMemory + 1) = color;
         vgaMemory += 2;
         str += 1;
         counter += 1;
     }
     while (counter % 80 != 0) {
-        *(vgaMemory + 1) = 0x0;
+        *(vgaMemory) = ' ';
+        *(vgaMemory + 1) = color;
         vgaMemory += 2;
         counter += 1;
     }
@@ -17,6 +18,9 @@ int print_vga(const char* str, int line) {
 }
 
 int main() {
-    print_vga("Kernel reached! :)", 0);
+    print_vga("Kernel reached! :)", 0, 0xe);
+    print_vga("", 1, 0x0);
+    print_vga("No matter where you go,", 2, 0x1e);
+    print_vga("    everyone is connected", 3, 0x1e);
     return 0;
 }
