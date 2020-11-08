c90fa28e25d65368bb4053b82e1bf45b1e044e54
Élise <atomheartother@gmail.com>
2020-11-08 14:22:22 +0100

    Basic keyboard driver

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,12 @@
 #include "screen.h"
 #include "isr.h"
 #include "timer.h"
+#include "keyboard.h"
 
 int kernel_main() {
     clearScreen();
     isrInstall();
     __asm__ __volatile__("sti");
     init_timer(50);
+    init_keyboard();
 }
