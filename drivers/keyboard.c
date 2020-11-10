699e79b2d59503e46ba62a6c1c43d91db8edae94
E. Navennec <atomheartother@gmail.com>
2020-11-10 20:21:49 +0100

    SC_MAX -> SC_COUNT

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -4,12 +4,12 @@
 #include "kstring.h"
 #include "screen.h"
 
-#define SC_MAX 57
+#define SC_COUNT 58
 
 #define BACKSPACE 0x0E
 #define ENTER 0x1C
 
-const char sc_ascii[SC_MAX + 1] = { '?', '?', '1', '2', '3', '4', '5', '6',     
+const char sc_ascii[SC_COUNT] = { '?', '?', '1', '2', '3', '4', '5', '6',     
     '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y', 
     'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G', 
     'H', 'J', 'K', 'L', ';', '\'', '`', '?', '\\', 'Z', 'X', 'C', 'V', 
@@ -18,7 +18,7 @@ const char sc_ascii[SC_MAX + 1] = { '?', '?', '1', '2', '3', '4', '5', '6',
 
 static void keyboardCallback() {
     unsigned char scancode = inb(0x60);
-    if (scancode > SC_MAX) return;
+    if (scancode >= SC_COUNT) return;
     if (scancode == ENTER) {
         newline();
     } else if (scancode == BACKSPACE) {
