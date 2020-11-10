1562b359399f5d12f5047428c65d955613ed5ef1
E. Navennec <atomheartother@gmail.com>
2020-11-10 23:02:59 +0100

    Add memcmp, memset, memmove
    The GCC documentation mentions that the memset, memcpy, memmove and
    memcmp functions must always be present. The compiler uses these
    automatically for certain optimization purposes and even code
    that doesn't use them can automatically generate calls to them.

--- libc/kmem.c
+++ libc/kmem.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "kmem.h"
 
 size_t placementAddress = 0;
@@ -6,6 +7,49 @@ void memcpy(void *dest, const void* src, size_t n) {
     while (n--) {
         *((char*)dest++) = *((char*)src++);
     }
+}
+
+// This function is guaranteed to function byte per byte
+// Right now it's the same as memcpy, but we could
+// optimize memcpy later
+static void fwdByteCopy(void *dest, const void* src, size_t n) {
+    memcpy(dest, src, n);
+}
+
+// Copies byte per byte, backwards
+static void bckByteCopy(void *dest, const void* src, size_t n) {
+    while (n--) {
+        *((char*)dest + n) = *((char*)src + n);
+    }
+}
+
+void memmove(void *dest, const void* src, size_t n) {
+    if (dest == src) return;
+    if ((dest > src  && (size_t)(dest - src) >= n) ||
+        (dest < src && (size_t)(src - dest) >= n)) {
+        // No overlap
+        memcpy(dest, src, n);
+    } else if (dest > src) {
+        bckByteCopy(dest, src, n);
+    } else {
+        fwdByteCopy(dest, src, n);
+    }
+}
+
+void memset(void *s, int c, size_t n) {
+    while (n--) {
+        *((char*)s++) = (char)c;
+    }
+}
+
+int memcmp(const void* s1, const void* s2, size_t n) {
+    const unsigned char *p1 = s1, *p2 = s2;
+    while (n-- > 0) {
+        if (*p1++ != *p2++) {
+            return *--p1 - *--p2;
+        }
+    }
+    return 0;
 }
 
 static void* kmalloc_internal(size_t size, int align, size_t* phys) {
--- libc/kmem.h
+++ libc/kmem.h
@@ -4,6 +4,9 @@
 #include <stddef.h>
 
 void memcpy(void *dest, const void* src, size_t n);
+void memset(void *s, int c, size_t n);
+void memmove(void *dest, const void* src, size_t n);
+int memcmp(const void* s1, const void* s2, size_t n);
 
 void* kmalloc_ap(size_t size, size_t* phys);
 void* kmalloc_p(size_t size, size_t* phys);
