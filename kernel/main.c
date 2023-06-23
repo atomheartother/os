341d8ca50de4041f4db46cb5b58b1778c8f66bcf
Élise N <atomheartother@gmail.com>
2023-06-23 01:09:40 -0400

    Explore memory map

--- kernel/main.c
+++ kernel/main.c
@@ -6,6 +6,11 @@
 #include "debug.h"
 #include "tar.h"
 
+extern uint32_t _kernelEnd;
+extern uint32_t _kernelStart;
+
+#define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
+
 uint32_t intFromTarOctal(char* buf, uint8_t size) {
   // Check the buffer is zero-terminated
   uint32_t n = 0;
@@ -35,36 +40,74 @@ int kernel_main(uint32_t magic, multiboot_info_t* mbi) {
       return 2;
     }
 
-    uint32_t i;
-    multibootModule* m;
-    tarHeader* initrdTar;
-    for (
-          i = 0, m = (multibootModule*) mbi->mods_addr;
-          i < mbi->mods_count;
-          i++, m++
-        ) {
-      initrdTar = (tarHeader*)m->start;
+
+    printMessage("flags: ");
+    printHex(mbi->flags);
+    newline();
+    printMessage("memLwr: ");
+    printHex(mbi->mem_lower);
+    newline();
+    printMessage("memUpr: ");
+    printHex(mbi->mem_upper + 1000);
+    newline();
+    printMessage("kStart: ");
+    printHex((uint32_t)&_kernelStart);
+    newline();
+    printMessage("kEnd:   ");
+    printHex((uint32_t)&_kernelEnd);
+    newline();
+    newline();
+    if (CHECK_FLAG(mbi->flags, 6)) {
+      printMessage("mmap length: ");
+      printHex(mbi->mmap_length);
+      newline();
+      printMessage("mmap addr: ");
+      printHex(mbi->mmap_addr);
+      newline();
+      newline();
+    }
+    if (CHECK_FLAG(mbi->flags, 7)) {
+      printMessage("drives_length: ");
+      printHex(mbi->drives_length);
+      newline();
+      printMessage("drives_addr: ");
+      printHex(mbi->drives_addr);
+      newline();
+      newline();
     }
 
-    while (*(initrdTar->name)) {
-      printMessage("name: ");
-      printMessage(initrdTar->name);
-      newline();
+    if (CHECK_FLAG(mbi->flags, 3)) {
+      uint32_t i;
+      multibootModule* m;
+      tarHeader* initrdTar;
+      for (
+            i = 0, m = (multibootModule*) mbi->mods_addr;
+            i < mbi->mods_count;
+            i++, m++
+          ) {
+        initrdTar = (tarHeader*)m->start;
+      }
 
-      char* tarContents = (char*)initrdTar + sizeof(tarHeader);
-      //printMessage("contents: ");
-      //printMessage(tarContents);
-      // newline();
+      while (*(initrdTar->name)) {
+        printMessage("name: ");
+        printMessage(initrdTar->name);
+        newline();
 
-      uint32_t size = intFromTarOctal(initrdTar->size, 12);
-      uint8_t blocks = (size/512) + 1;
+        char* tarContents = (char*)initrdTar + sizeof(tarHeader);
+        //printMessage("contents: ");
+        //printMessage(tarContents);
+        // newline();
 
-      printMessage("size: ");
-      printHex(size);
+        uint32_t size = intFromTarOctal(initrdTar->size, 12);
+        uint8_t blocks = (size/512) + 1;
 
-      newline();
-      newline();
-      initrdTar = (tarHeader*)(tarContents + 512 * blocks);
+        printMessage("size: ");
+        printHex(size);
+
+        newline();
+        newline();
+        initrdTar = (tarHeader*)(tarContents + 512 * blocks);
+      }
     }
     //printMessage("Welcome to lizOS!");
     //newline();
